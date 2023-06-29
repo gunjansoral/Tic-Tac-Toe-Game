784a861249358e95fcc9c0bb876e1a601e8ba7a2
gunjan <gunjan.soral@gmail.com>
2023-06-29 11:20:43 +0530

    server deployed

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+require('dotenv').config();
+const { PORT, FRONTEND_URI } = process.env
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:3000"
+    origin: FRONTEND_URI
   }
 });
 
@@ -14,7 +16,6 @@ io.on("connection", (socket) => {
   socket.emit('init', { data: 'hello world' });
 });
 
-const PORT = 8000;
 httpServer.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 });
