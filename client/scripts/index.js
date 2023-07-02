025bf13c4f4a142f88ca0e6e67610efc4a7558fb
gunjan <gunjan.soral@gmail.com>
2023-07-02 17:51:59 +0530

    edited-v1

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -14,11 +14,24 @@
 
 // Objects -> Game, Board, Block, Player.
 
-import Game from './Game.js'
+import Game from './Game.js';
 import Player from "./Player.js";
 
 //Initialize game
 const player1 = new Player('player 1', 'X')
 const player2 = new Player('player 2', 'O')
+
+const newRoom = () => {
+  const roomId = Math.floor(Math.random() * 100000000); // Generate a random number between 0 and 99999999
+  return roomId.toString().padStart(8, '0'); // Convert the number to a string and pad it with leading zeros if necessary
+};
+
+const newGame = async () => {
+  const roomId = newRoom();
+  console.log('Room Id: ' + roomId);
+}
+
+newGame()
+
 export const game = new Game(player1, player2);
 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require('cors');
 require('dotenv').config();
+
 const { PORT, FRONTEND_URI } = process.env
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
+const server = createServer(app);
+app.use(cors());
+
+const io = new Server(server, {
   cors: {
-    origin: FRONTEND_URI
+    origin:
+      // FRONTEND_URI ||
+      'http://127.0.0.1:5501'
   }
 });
 
@@ -16,6 +22,6 @@ io.on("connection", (socket) => {
   socket.emit('init', { data: 'hello world' });
 });
 
-httpServer.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 });
